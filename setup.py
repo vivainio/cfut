13ef40f2764750da08a31823b29c14c3ec40a91c
Ville M. Vainio <ville.vainio@basware.com>
2021-03-18 17:33:36 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="cfut",
-    version="1.4.2",
+    version="1.4.3",
     description="cfut is a wrapper cli for 'aws cloudformation', 'aws ecr' and some others",
     author="Ville M. Vainio",
     author_email="ville.vainio@basware.com",
